797c4db9d9321401bd91de60ff0967c52a3a3e39
twins-slayer <bartolomeo.jakubiak@gmail.com>
2018-02-15 14:49:27 +0100

    part II and some refactoring in part I

--- day02/day02.py
+++ day02/day02.py
@@ -1,17 +1,25 @@
+
 'part I'
 
 with open("numbers.txt", "r") as f:
-	data = f.read().strip()
+	checksum = 0
 
-checksum = 0
-
-for line in data.split('\n'):
-	num = [int(x) for x in line.split('\t')]
-	checksum += max(num) - min(num)
-print (checksum)
+	for line in f:
+		num = [int(x) for x in line.split('\t')]
+		checksum += max(num) - min(num)
+	print (checksum)
 
 'part II'
 
 with open("numbers.txt", "r") as f:
-	data = f.read().strip()
+
+	result = 0
+	for line in f:
+		num = [int(x) for x in line.split('\t')]
+		for a in num:
+			for b in num:
+				if a != b and a % b == 0:
+					result += a / b
+	print (result)
+	
 
